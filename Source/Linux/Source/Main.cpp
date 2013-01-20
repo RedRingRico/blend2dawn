3ae7de3bde0c3ece912b6e87ff3bff33d38c4ab4
Rico <teh_programerer@interactivelaboratories.com>
2013-01-20 02:07:15 +0000

    |SOURCE| [Main] Using Git version information

--- Source/Linux/Source/Main.cpp
+++ Source/Linux/Source/Main.cpp
@@ -1,12 +1,13 @@
 #include <ncurses.h>
 #include <BlenderFile.hpp>
-#include <HgVersion.hpp>
+#include <GitVersion.hpp>
 
 int main( int p_Argc, char **p_ppArgv )
 {
 	BlenderFile File;
 	initscr( );
-	printw( "blend2dawn\n[Ver. 0.0.0.%d | %s]\n", HG_REVISION, HG_CHANGESET );
+	printw( "blend2dawn\n[Ver. 0.0.0.%d | %s]\n", GIT_ROLLINGCOUNT,
+		GIT_COMMITHASH );
 
 	if( p_Argc > 1 )
 	{
